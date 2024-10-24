2f1fc0af0a1b8624129a33c0123f14c47a058727
knightoffcode <isaiaspereira103@yahoo.com>
2024-10-24 12:40:04 -0300

    #004 - Removing unecessary header code.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-import { BiUserCircle, BiMapPin, BiSearch } from 'react-icons/bi'
-import { IoMdLogOut } from 'react-icons/io'
-import { FiMapPin, FiGlobe, FiDollarSign } from 'react-icons/fi'
-import { CiMenuBurger } from 'react-icons/ci'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
-import { GrUpdate, GrMenu } from 'react-icons/gr'
+import { BiMapPin, BiSearch } from 'react-icons/bi';
+import { CiMenuBurger } from 'react-icons/ci';
+import { GrUpdate } from 'react-icons/gr';
 
-import Logo from "../../assets/Logo.png"
+import Logo from "../../assets/Logo/Logo.png";
 
 /**
  *
@@ -92,52 +89,13 @@ function Header() {
                         Informe seu CEP
                     </button>
                     <ul className='header__menu__list'>
-                        {/* <div className='header__menu__item'>
-                                <li className='header__item'>
-                                    <a
-                                        href='{page.url}'
-                                        className='header__link header__link--all'
-                                    >
-                                        <GrMenu className='header__link__icon' />
-                                        Ver todas as categorias
-                                    </a>
-                                </li>
-                                <div className='header__submenu'>
-                                    <ul className='header__submenu__list'>
-                                        <li className='header__submenu__item'>
-                                            <a
-                                                className='header__submenu__anchor'
-                                                href='{subpage.url}'
-                                            ></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div> */}
-
                         {menuPages.map((page, index) => (
                             <div key={index} className='header__menu__item'>
                                 <li className='header__item'>
                                     <a href={page.url} className='header__link'>
-                                        {/* <FaAngleDown className="header__link__icon" /> */}
                                         {page.page}
                                     </a>
                                 </li>
-                                {page.subPages
-                                    ? page.subPages.map((subpage, subIndex) => (
-                                        <div className='header__submenu' key={subIndex}>
-                                            <ul className='header__submenu__list'>
-                                                <li className='header__submenu__item'>
-                                                    <a
-                                                        className='header__submenu__anchor'
-                                                        href={subpage.url}
-                                                    >
-                                                        {subpage.name}
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    ))
-                                    : null}
                             </div>
                         ))}
                     </ul>
@@ -148,14 +106,6 @@ function Header() {
                 </nav>
                 <nav className='header__nav'>
                     <ul className='header__list'>
-                        {/* <li className="header__item header__item--user">
-                                <a href="/contact" className="header__link header__link--phone">
-                                +55 11 9999 9999
-                                <span>
-                                support 24/7
-                                </span>
-                                </a>
-                            </li> */}
                         <li className='header__item header__item--user'>
                             <div className='header__search'>
                                 <input
@@ -168,21 +118,6 @@ function Header() {
                                 <BiSearch className='header__link__icon header__link__icon--user' />
                             </a>
                         </li>
-                        {/* <li className='header__item header__item--user'>
-                                <a href='/contact' className='header__link header__link--user'>
-                                    <BiUserCircle className='header__link__icon header__link__icon--user' />
-                                </a>
-                            </li>
-                            <li className='header__item header__item--user'>
-                                <a href='/about' className='header__link header__link--user'>
-                                    <IoMdLogOut className='header__link__icon header__link__icon--user' />
-                                </a>
-                            </li>
-                            <li className='header__item header__item--user'>
-                                <a href='/cart' className='header__link header__link--user'>
-                                    <AiOutlineShoppingCart className='header__link__icon header__link__icon--user' />
-                                </a>
-                            </li> */}
                     </ul>
                     <button className='header__menumobile'>
                         <CiMenuBurger />
@@ -191,7 +126,7 @@ function Header() {
 
             </div>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
