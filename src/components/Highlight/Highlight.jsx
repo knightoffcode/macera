60c4c7021a555ebec50ae520f2a9253243fb0ad5
knightoffcode <isaiaspereira103@yahoo.com>
2024-10-25 13:16:55 -0300

    #008 - Scroll bar progress logic.

--- src/components/Highlight/Highlight.jsx
+++ src/components/Highlight/Highlight.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import Headline from "../Headline/";
 import AtractiveImages from '../AtractiveImages/';
@@ -12,10 +12,34 @@ import Blog from '../Blog/';
  */
 
 function Highlight() {
+    const wrapperRef = useRef(null);
+    const [scrollProgress, setScrollProgress] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (wrapperRef.current) {
+                const { scrollTop, scrollHeight, clientHeight } = wrapperRef.current;
+                const totalScroll = scrollHeight - clientHeight;
+                const scrollPosition = scrollTop / totalScroll;
+                setScrollProgress(scrollPosition * 100);
+            }
+        };
+
+        const wrapperElement = wrapperRef.current;
+        if (wrapperElement) {
+            wrapperElement.addEventListener('scroll', handleScroll);
+        }
+
+        return () => {
+            if (wrapperElement) {
+                wrapperElement.removeEventListener('scroll', handleScroll);
+            }
+        };
+    }, []);
  
     return (
         <div className="highlight__section">
-            <div className="highlight__wrapper">
+            <div className="highlight__wrapper" ref={wrapperRef}>
                
                 <Headline />
                 <AtractiveImages />
@@ -26,7 +50,7 @@ function Highlight() {
             </div>
 
             <div className="progress__bar">
-                <div className="progress__fill"></div>
+                <div className="progress__fill" style={{ height: `${scrollProgress}%` }}></div>
             </div>
         </div>
     );
